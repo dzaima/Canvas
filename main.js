faca49246e050be7093d7e1b047ece117d71ff9b
dzaima <atdzim@inbox.lv>
2018-03-03 00:16:28 +0200

    ಠ_ಠ

--- main.js
+++ main.js
@@ -525,7 +525,7 @@ async function run (program, inputs) {
     },
     "╵": {
       N: (n) => n.plus(1),
-      S: (s) => s.replace(/(^|[.?!]\s*)(\w)/g, (a,b,c)=>b+c.toUpperCase())
+      S: (s) => s.replace(/(^|[.?!]\s*)(\w)/g, (a,b,c)=>b+c.toUpperCase()),
     },
     "╷": {
       N: (n) => n.minus(1),
